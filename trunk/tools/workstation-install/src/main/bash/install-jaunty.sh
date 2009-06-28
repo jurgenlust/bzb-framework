d51c73fb74e6624e7a31394d56f41a27de96a331
Jurgen.Lust <Jurgen.Lust@b98e820e-29be-11de-aa1e-f37fc001f1d9>
2009-06-28 17:35:33 +0000

    added more comments, replaced MySQL with PostgreSQL and added Apache, PHP and Tomcat

--- trunk/tools/workstation-install/src/main/bash/install-jaunty.sh
+++ trunk/tools/workstation-install/src/main/bash/install-jaunty.sh
@@ -6,7 +6,7 @@
 # This script prepares an Ubuntu Linux 9.04 workstation for developing
 # web applications using the BZB Framework
 #
-# Jurgen Lust, April 19, 2009
+# Jurgen Lust, June 28, 2009
 # Jurgen.Lust@gmail.com
 #
 ###################################################
@@ -18,7 +18,7 @@ userName=$LOGNAME
 userHome=$HOME
 javaHome="/usr/lib/jvm/java-6-sun"
 optDir="/opt"
-eclipseDownloadUrl="http://eclipse.mirror.kangaroot.net/technology/epp/downloads/release/ganymede/SR2/eclipse-jee-ganymede-SR2-linux-gtk.tar.gz"
+eclipseDownloadUrl="http://eclipse.a3-system.be/technology/epp/downloads/release/galileo/R/eclipse-jee-galileo-linux-gtk-x86_64.tar.gz"
 eclipseDownloadFile="/tmp/eclipse.tar.gz"
 eclipseHome="/opt/eclipse"
 javaMajorVersion=6
@@ -39,29 +39,74 @@ sudo apt-get update
 #install latest versions of everything
 sudo apt-get upgrade -y
 
-#install some required packages
-echo "Installing required packages"
+#install a database
+#uncomment this if you want MySQL
+#sudo apt-get install mysql-server -y
+#sudo apt-get install mysql-admin -y
+#we use PostgreSQL by default
+sudo apt-get install postgresql -y
+sudo apt-get install pgadmin3 -y
+
+#We still use the Sun JDK
 sudo apt-get install "sun-java6-jdk" -y
 sudo apt-get install "sun-java6-fonts" -y
 sudo apt-get install "sun-java6-plugin" -y
+
+#CURL is required for downloading some stuff later on
 sudo apt-get install curl -y
+
+#Ant is in every Java developer's toolbox
 sudo apt-get install ant -y
-sudo apt-get install subversion -y
-sudo apt-get install nautilus-open-terminal -y
-sudo apt-get install texlive-full -y
-sudo apt-get install lat -y
-sudo apt-get install mysql-server -y
-sudo apt-get install mysql-admin -y
-sudo apt-get install netbeans -y
+
+#So is Maven 2
 sudo apt-get install maven2 -y
 
-#install optional stuff
-sudo apt-get install cups-pdf -y
+#So is Subversion
+sudo apt-get install subversion -y
+
+#So is Tomcat 6
+sudo apt-get install tomcat6
+
+#Very handy nautilus plugin
+sudo apt-get install nautilus-open-terminal -y
+
+#If you don't like NetBeans, comment this
+sudo apt-get install netbeans -y
+
+#Nice tool for editing CSS files
+sudo apt-get install cssed -y
+
+#Apache is always good to have
+sudo apt-get install apache2 -y
+sudo apt-get install libapache2-mod-jk -y
+
+#Do we want remote access to this machine?
+sudo apt-get install openssh-server -y
+sudo apt-get install samba -y
+
+
+#If you don't want PHP, comment this
+sudo apt-get install libapache2-mod-php5 -y
+sudo apt-get install php5 -y
+sudo apt-get install php5-gd -y
+sudo apt-get install php5-pgsql -y
+sudo apt-get install php5-mysql -y
+
+#some nice browser plugins
 sudo apt-get install firefox-webdeveloper -y
 sudo apt-get install mozilla-livehttpheaders -y
 sudo apt-get install flashplugin-nonfree -y
 
-#install eid stuff
+#password manager: if you maintain a number of websites, this comes in very handy
+sudo apt-get install revelation -y
+
+#LDAP administration tool
+sudo apt-get install lat -y
+
+#Yes, I still like LaTeX
+sudo apt-get install texlive-full -y
+
+#install eid stuff - only useful in Belgium really
 sudo apt-get install libccid -y
 sudo apt-get install beidgui -y
 sudo apt-get install beid-tools -y
@@ -88,7 +133,7 @@ echo 'Name=Eclipse' >> /tmp/eclipse.desktop
 echo 'Type=Application' >> /tmp/eclipse.desktop
 echo 'StartupNotify=true' >> /tmp/eclipse.desktop
 echo 'Terminal=false' >> /tmp/eclipse.desktop
-echo 'Comment=Eclipse Europa' >> /tmp/eclipse.desktop
+echo 'Comment=Eclipse Galileo' >> /tmp/eclipse.desktop
 echo "Exec=$eclipseHome/eclipse" >> /tmp/eclipse.desktop
 echo "TryExec=$eclipseHome/eclipse" >> /tmp/eclipse.desktop
 echo "Icon=$eclipseHome/icon.xpm" >> /tmp/eclipse.desktop
@@ -103,12 +148,8 @@ mkdir -p "$userHome/.m2/repository"
 
 #setting up environment
 echo "Setting up environment for user $userName"
-echo "NLS_LANG=Dutch_Belgium.AL32UTF8" > $bzbProfile
-echo "NLS_SORT=Dutch" >> $bzbProfile
-echo "NLS_LENGTH_SEMANTICS=CHAR" >> $bzbProfile
 echo "JAVA_HOME=$javaHome" >> $bzbProfile
 echo "PATH=$javaHome/bin:$eclipseHome:\$PATH" >> $bzbProfile
-echo "export NLS_LANG NLS_SORT NLS_LENGTH_SEMANTICS" >> $bzbProfile
 echo "export JAVA_HOME PATH" >> $bzbProfile
 
 echo "" >> $profile
@@ -116,10 +157,6 @@ echo "#Append settings for BZB Framework" >> $profile
 echo ". $bzbProfile" >> $profile
 
 source $bzbProfile
-
-mkdir -p ~/Development/Java
-mkdir -p ~/Development/workspaces
-mkdir -p ~/conf
 
 #permissions fix. If we don't do this, X session immediately quits
 sudo chmod -R a+w /tmp
