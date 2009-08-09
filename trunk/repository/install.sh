764294228edbd286953fba14e413848fe0e3bb50
Jurgen.Lust <Jurgen.Lust@b98e820e-29be-11de-aa1e-f37fc001f1d9>
2009-08-09 22:00:58 +0000

    no longer needed since we now have a missing resources repository on google code

--- trunk/repository/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-mvn install:install-file -DgroupId=net.sourceforge.jsfcomp -DartifactId=chartcreator -Dversion=1.2.0 -Dpackaging=jar -DgeneratePom=true -Dfile=chartcreator-1.2.0.jar
-mvn install:install-file -DgroupId=net.java.dev.pdfrenderer -DartifactId=pdfrenderer -Dversion=20080921 -Dpackaging=jar -DgeneratePom=true -Dfile=PDFRenderer.jarmvn install:install-file -DgroupId=net.java.dev.weblets -DartifactId=weblets-api -Dversion=1.1 -Dpackaging=jar -DgeneratePom=true -Dfile=weblets-api-1.1.jarmvn install:install-file -DgroupId=net.java.dev.weblets -DartifactId=weblets-impl -Dversion=1.1 -Dpackaging=jar -DgeneratePom=true -Dfile=weblets-impl-1.1.jar
